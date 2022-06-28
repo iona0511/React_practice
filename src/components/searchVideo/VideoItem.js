9d9b1997b5e9068ba08fb049bf02f2e24e18c3d0
iona0511 <lilly800511@gmail.com>
2022-06-28 21:05:25 +0800

    youtube componentDidMount

--- src/components/searchVideo/VideoItem.js
+++ src/components/searchVideo/VideoItem.js
@@ -1,10 +1,10 @@
 import './VideoItem.css';
 import React from "react";
 
-const VideoItem = ({video})=>{
+const VideoItem = ({video ,onVideoSelect})=>{
     return (
-    <div className="video-item item">
-        <img className=" ui image" src ={video.snippet.thumbnails.medium.url}/>
+    <div className="video-item item" onClick={()=>{onVideoSelect(video)}}>
+        <img alt={video.snippet.title} className=" ui image" src ={video.snippet.thumbnails.medium.url}/>
         <div className="content">        
             <div className="header">{video.snippet.title}</div>
         </div>
@@ -12,4 +12,4 @@ const VideoItem = ({video})=>{
         )
 };
 
-export default VideoItem;
+export default VideoItem; 
--- src/components/searchVideo/VideoList.js
+++ src/components/searchVideo/VideoList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList =({videos})=>{
+const VideoList =({videos, onVideoSelect })=>{
    const renderList =  videos.map(video=>{
-        return <VideoItem video={video}/>
+        return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video}/>
+                                                    // 參數名稱  value
     })
     return(
         <div>{renderList}</div>
