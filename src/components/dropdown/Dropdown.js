370bf218706e92ca2b25a9effcc3641c58d32c6b
iona0511 <lilly800511@gmail.com>
2022-07-03 01:37:22 +0800

    onclick for options

--- src/components/dropdown/Dropdown.js
+++ src/components/dropdown/Dropdown.js
@@ -1,23 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
                     // 這裡的selected & onSelectedChange 是App.js的參數
 const Dropdown = ({options,selected,onSelectedChange})=>{
+    const[open,setOpen]=useState(false);
     const renderedOptions = options.map((option)=>{
+        if(option.value===selected.value){
+            // return null 代表不render
+            return null
+        }
         return (
-            <div key={option.value} className="item">{option.label}</div>
-
+            <div key={option.value} 
+            className="item"
+            onClick={()=>{onSelectedChange(option)}}
+            >{option.label}</div>
         );
-
-    })
+    });
 
 
     return(
     <div className="ui form">
         <div className="field">
             <label className="label">select a color</label>
-            <div className="ui selection dropdown visible active">
+            <div onClick={()=>setOpen(!open)} 
+            className={`ui selection dropdown ${open ? 'visible active': ''}`}>
                 <i className="dropdown icon"></i>
                 <div className="text">{selected.label}</div>
-                <div className="menu visible transition">{renderedOptions}</div>
+                <div className={`menu ${open?'visible transition':''}`}>{renderedOptions}</div>
             </div>
         </div>
     </div>
