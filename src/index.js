fb28b25db49164f0879c78c80795a3573f9e68d6
iona0511 <lilly800511@gmail.com>
2022-07-04 16:02:04 +0800

    -

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 // import App from './components/searchBar/App';
 // import App from './components/searchVideo/App';
 // import App from './components/accordion/App';
- import App from './components/dropdown/App';
+//  import App from './components/dropdown/App';
+ import App from './components/path/App';
+
 
 
 ReactDOM.render(<App />, document.querySelector('#root'));
