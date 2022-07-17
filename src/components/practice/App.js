65bd47dadf4a589e6a7d2ed83af9a299f7bb771c
iona0511 <lilly800511@gmail.com>
2022-07-17 14:17:37 +0800

    practice for array

--- src/components/practice/App.js
+++ src/components/practice/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Count from './Count';
 import JSX from './JSX';
+import HandleArray from "./HandleArray";
 
 const App = () => {
     return (<>
         <Count />
         <JSX />
+        <HandleArray />
     </>
     );
 };
--- src/components/practice/JSX.js
+++ src/components/practice/JSX.js
@@ -24,7 +24,7 @@ function JSX() {
             console.log(123);
         }}
 
-
+        <hr />
 
     </>);
 }
